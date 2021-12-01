d587fa0d39f228e9da3db80c23bc3ace458fe05c
Brian Branca <brianbranca16@gmail.com>
2021-12-01 22:50:08 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Main from "./Main";
+
+export default function App() {
+  const [darkMode, setDarkMode] = React.useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
+  return (
+    <div className="container">
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
+    </div>
+  );
+}
